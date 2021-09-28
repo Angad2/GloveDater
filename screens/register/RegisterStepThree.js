b62e3f8cc83cf66cf27198eb72c7f886a4d2e4dd
info-cbnits <44164844+info-cbnits@users.noreply.github.com>
2021-09-28 19:47:00 +0530

    Add files via upload

--- screens/register/RegisterStepThree.js
+++ screens/register/RegisterStepThree.js
@@ -6,6 +6,7 @@ import rstyles from '../rstyles';
 //import Colors from '../../constants/color';
 
 import Header from '../../components/Header';
+//import axios from 'axios';
 
 const RegisterStepThree = props => {
 
@@ -18,12 +19,21 @@ const RegisterStepThree = props => {
     const [intent, _intent] = useState()
 
     const intens = [
-        {"dating":"Dating"},
-        {"travel":"Travel"},
-        {"dating":"Dating"},
-        {"dating":"Dating"},
+        {"name":"dating","value":"Dating"},
+        {"name":"travel","value":"Travel"},
+        {"name":"experiences","value":"Experiences"},
+        {"name":"romance","value":"Romance"},
+        {"name":"emotional","value":"Emotional"},
+        {"name":"open","value":"Open"},
+        {"name":"nsa","value":"NSA"},
+        {"name":"fwb","value":"FWB"},
+        {"name":"shortterm","value":"Short term"},
+        {"name":"longterm","value":"Longterm"},
+        {"name":"arrangement","value":"Arrangement"},
+    ]
 
-    ]   
+    const [intentArr, _intentArr] = useState([])
+
   const countryHandler = (enteredCountry) => {
     _country(enteredCountry);
 };
@@ -33,8 +43,11 @@ const cityHandler = (enteredCity) => {
 };
 
 const selectIntent = (enterIntent) => {
-    _intent(enterIntent);
-    Object.value;
+    console.log(enterIntent);
+    //_intent(enterIntent);
+    //_intentArr.push(_intentArr)
+    //console.log(intentArr);
+
   };
 
   React.useEffect(()=>{
@@ -42,7 +55,6 @@ const selectIntent = (enterIntent) => {
     console.log(props.navigation.state.params.param.password, "+++++++props pass"),
     console.log(props.navigation.state.params.param.gender, "+++++++props gender"),
     console.log(props.navigation.state.params.param.looking, "+++++++props looking for"),
-    console.log(intens, "++++++++++++++++++++++++intens")
     _email(props.navigation.state.params.param.email),
     _password(props.navigation.state.params.param.password),
     _gender(props.navigation.state.params.param.gender),
@@ -77,98 +89,13 @@ const selectIntent = (enterIntent) => {
                 </View>
                 <Text style={rstyles.formviewtxt}>Intent</Text>
                 <View style={rstyles.intentview}>
-                    {/* {intens && (
-                        {
-                            intens.map(inten =>{
-                                console.log(inten, "++++++++++++++++++++inten")
-                                (
-                                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                                        <Text style={rstyles.chktxt}>Dating</Text>
+                    {intens && 
+                            intens.map(inten =>
+                                    <TouchableOpacity  onPress={()=>selectIntent(inten.name)} style={rstyles.intentchk}>
+                                        <Text style={rstyles.chktxt}>{inten.name}</Text>
                                     </TouchableOpacity> 
-                                )
-                            })
+                            )       
                         }
-                    )
-
-                    } */}
-                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                        <Text style={rstyles.chktxt}>Dating</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                        <Text style={rstyles.chktxt}>Travel</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                        <Text style={rstyles.chktxt}>Experiences</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                        <Text style={rstyles.chktxt}>Romance</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                        <Text style={rstyles.chktxt}>Emotional connection</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                        <Text style={rstyles.chktxt}>Open</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                        <Text style={rstyles.chktxt}>NSA</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                        <Text style={rstyles.chktxt}>FWB</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                        <Text style={rstyles.chktxt}>Short term</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                        <Text style={rstyles.chktxt}>Long term</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                        <Text style={rstyles.chktxt}>Arrangement</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                        <Text style={rstyles.chktxt}>NSA</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                        <Text style={rstyles.chktxt}>FWB</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                        <Text style={rstyles.chktxt}>Short term</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                        <Text style={rstyles.chktxt}>Long term</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                        <Text style={rstyles.chktxt}>Arrangement</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                        <Text style={rstyles.chktxt}>NSA</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                        <Text style={rstyles.chktxt}>FWB</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                        <Text style={rstyles.chktxt}>Short term</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                        <Text style={rstyles.chktxt}>Long term</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                        <Text style={rstyles.chktxt}>Arrangement</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                        <Text style={rstyles.chktxt}>NSA</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                        <Text style={rstyles.chktxt}>FWB</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                        <Text style={rstyles.chktxt}>Short term</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                        <Text style={rstyles.chktxt}>Long term</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity  onPress={selectIntent} style={rstyles.intentchk}>
-                        <Text style={rstyles.chktxt}>Arrangement</Text>
-                    </TouchableOpacity>
                 </View>
             </ScrollView>
             <View style={rstyles.btnview}>
@@ -183,7 +110,33 @@ const selectIntent = (enterIntent) => {
                     "city": city,
                     "intent": intent
                     }
-                })}
+                })
+                // axios.post("http://111.93.169.90:8484/V1/Signup",
+                // {
+                //     "Email":"email",
+                //     "Gender":"Male",
+                //     "Looking_to_date_a":"Women",
+                //     "Country":"India",
+                //     "City":"Kolkata",
+                //     "Age":"35",
+                //     "Body":"Tall",
+                //     "Height":"175cm",
+                //     "Hair":"Black",
+                //     "Ethnicity":"saSKASA DKWLK",
+                //     "Intent_option":"abc",
+                //     "About_me":"bfshdfh jsdjkashdj jasdjajdajd",
+                //     "Looking_for":"Women",
+                //     "Favorite_travel_spot":"Kashmir",
+                //     "Favorite_restaurnt":"Mehake E Punjab",
+                //     "Future_dream_experience":"djsd jdjawd kjjwjejw kjweje",
+                //     "Password":"1234567",
+                //     "Intent":"Dating",
+                //     "Photo":""
+                // }
+                // ).then(result=>{
+                //     console.log(result)
+               // })
+            }
                 style={rstyles.btncontainer2}>
                     <Text style={rstyles.btntext}>Continue</Text>
                 </TouchableOpacity>
